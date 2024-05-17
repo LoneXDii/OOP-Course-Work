8349663a1dbe780b25a20b021be35d64ad091b93
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-05-17 22:38:12 +0300

    Implemented user searching on FindUserPopup

--- Client/Client/Popups/FindUserPopup.xaml.cs
+++ Client/Client/Popups/FindUserPopup.xaml.cs
@@ -1,3 +1,4 @@
+using Client.Domain.Entitites;
 using Client.Persistence.Repositories;
 using CommunityToolkit.Maui.Views;
 
@@ -7,10 +8,24 @@ namespace Client.Popups;
 public partial class FindUserPopup : Popup
 {
 	private readonly IUnitOfWork _unitOfWork;
+	private List<User> _users;
 	public FindUserPopup(IUnitOfWork unitOfWork)
 	{
 		InitializeComponent();
 		_unitOfWork = unitOfWork;
-		usersView.ItemsSource = _unitOfWork.AllUsers();
+		_users = _unitOfWork.AllUsers();
+	}
+
+	private void OnSearchButonTextChanged(object sender, EventArgs e)
+	{
+		if (userSearchBar.Text != "")
+		{
+			usersView.IsVisible = true;
+            usersView.ItemsSource = _users.Where(x => x.Name.StartsWith(userSearchBar.Text, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+		else
+		{
+			usersView.IsVisible = false;
+		}
 	}
 }
