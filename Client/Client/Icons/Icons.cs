27c9b279b436659cf5913e98eb09823994995b47
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-05-21 23:04:52 +0300

    Fixed pages titles, added icons to buttons

---
+++ Client/Client/Icons/Icons.cs
@@ -0,0 +1,17 @@
+﻿namespace Client.Icons;
+
+
+public static class Icons
+{
+    public const string Add = "\uf5a2";
+    public const string AddCircle = "\uf00d";
+    public const string Edit = "\uf3db";
+    public const string EditOff = "\ue5c3";
+    public const string Send = "\uf6a2";
+    public const string Delete = "\uf34c";
+    public const string Chat = "\ue2f8";
+    public const string PersonAdd = "\uf5cc";
+    public const string PersonDelete = "\uf5da";
+    public const string Cancel = "\uf3f2";
+    public const string LogOut = "\ue935";
+}
